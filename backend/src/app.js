bf79d8b7baa108759dc713eee7a5f163af46ee71
vedansh tiwari <vt2855028@gmail.com>
2025-02-24 09:50:29 +0530

    after deployed

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173",["*"]],
+    origin: ["https://kvtapi.vedanshtiwari.tech", ["*"]],
     credentials: true,
   })
 );
